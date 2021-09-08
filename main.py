c0c022980cfb731dd47b945865e233a7c31404cd
Jay Pate <pateljay24004@gmail.com>
2021-09-08 12:21:09 +0530

    claen up

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ from pathlib import Path
 from discord_slash import SlashCommand
 from discord_slash.context import MenuContext
 from discord_slash.model import ContextMenuType
+from discord_slash.utils.manage_commands import create_option, create_choice, create_permission
+from discord_slash.model import SlashCommandPermissionType
 # Local code
 import utils.json_loader
 
@@ -33,6 +35,19 @@ load_dotenv()
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
 print(f"{cwd}\n-----")
+
+
+staff_perm = {
+    829615142450495601:[
+    create_permission(567374379575672852, SlashCommandPermissionType.USER, True),
+    create_permission(488614633670967307, SlashCommandPermissionType.USER, True),
+    create_permission(829615142450495609, SlashCommandPermissionType.ROLE, True),
+    create_permission(829615142450495608, SlashCommandPermissionType.ROLE, True),
+    create_permission(829615142450495607, SlashCommandPermissionType.ROLE, True),
+    create_permission(829615142450495603, SlashCommandPermissionType.ROLE, True)
+    ]
+
+}
 
 
 intents = discord.Intents.all()  # Help command requires member intents
@@ -94,34 +109,6 @@ async def amari(ctx, user:discord.Member=None):
     amari_level = await amari.fetch_user(ctx.guild.id, user.id)
 
     await ctx.send(f"User Name: `{user.display_name}'\nAmari Level: {amari_level.level}\nWeekly Amari: {amari_level.weeklyexp}")
-
-@slash.context_menu(target=ContextMenuType.MESSAGE,
-                    name="whois",
-                    guild_ids=[829615142450495601])
-async def whois(ctx: MenuContext):
-    def fomat_time(time):
-        return time.strftime('%d-%B-%Y %I:%m %p')
-
-    member = ctx.target_message.author
-    usercolor = member.color
-
-    today = (datetime.datetime.utcnow() - member.created_at).total_seconds()
-
-    embed = discord.Embed(title=f'{member.name}', color=usercolor)
-    embed.set_thumbnail(url=member.avatar_url)
-    embed.add_field(name='Account Name:', value=f'{member.name}', inline=False)
-    embed.add_field(name='Created at:', value=f"{fomat_time(member.created_at)}\n{format_timespan(today)}")
-    embed.add_field(name='Joined at', value=fomat_time(member.joined_at))
-    embed.add_field(name='Account Status', value=str(member.status).title())
-    embed.add_field(name='Account Activity', value=f"{str(member.activity.type).title().split('.')[1]} {member.activity.name}" if member.activity is not None else "None")
-
-    hsorted_roles = sorted([role for role in member.roles[-2:]], key=lambda x: x.position, reverse=True)
-
-
-    embed.add_field(name='Top Role:', value=', '.join(role.mention for role in hsorted_roles), inline=False)
-    embed.add_field(name='Number of Roles', value=f"{len(member.roles) -1 }")
-    embed.set_footer(text=f'ID {member.id}', icon_url=member.avatar_url)
-    await ctx.send(embed=embed, hidden=True)
 
 if __name__ == "__main__":
     # When running this file, if it is the 'main' file
