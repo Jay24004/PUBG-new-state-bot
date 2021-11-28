5b60ec6257040447b39ac9cf2a7232e5c072c464
Jay Patel <pateljay24004@gmail.com>
2021-11-28 20:36:56 +0530

    added tag system

--- cogs/tags.py
+++ cogs/tags.py
@@ -1,113 +1,146 @@
-import asyncio
-from datetime import datetime
-import re
 import discord
-import datetime
-from discord import guild
-from discord import embeds
+from discord import user
 from discord.ext import commands
-from discord_slash.utils.manage_commands import create_option, create_choice
-from discord_slash import cog_ext, SlashContext, cog_ext, SlashContext
+from discord.ext.commands.core import check, guild_only
+from discord_slash.context import MenuContext
+from discord_slash.model import ContextMenuType
 from discord_slash.utils.manage_commands import create_option, create_choice, create_permission
 from discord_slash.model import SlashCommandPermissionType
-from discord_slash.utils.manage_components import wait_for_component
-from discord_slash.context import ComponentContext
-from utils.util import Pag
-import re
+from discord_slash import cog_ext, SlashContext, cog_ext, SlashContext
+import asyncio
+import datetime
 from discord_slash.utils.manage_components import create_button, create_actionrow
 from discord_slash.model import ButtonStyle
+from utils.util import Pag
+from discord_slash.utils.manage_components import wait_for_component
+from discord_slash.context import ComponentContext
 
 guild_ids=[814374218602512395]
 
-admin_perms = {
-    814374218602512395:
-    [
+perms_a = {
+    814374218602512395: [
         create_permission(894123602918670346, SlashCommandPermissionType.ROLE, True),
         create_permission(814405582177435658, SlashCommandPermissionType.ROLE, True),
+        create_permission(909333702683799562, SlashCommandPermissionType.ROLE, True)
     ]
 }
 
-staff_perms = {
-    814374218602512395:
-
-    [   
+perms_staff = {
+    814374218602512395: [
         create_permission(894123602918670346, SlashCommandPermissionType.ROLE, True),
         create_permission(814405582177435658, SlashCommandPermissionType.ROLE, True),
-        create_permission(814405684581236774, SlashCommandPermissionType.ROLE, True),
-        create_permission(816595613230694460, SlashCommandPermissionType.ROLE, True)
+        create_permission(909333702683799562, SlashCommandPermissionType.ROLE, True),
+        create_permission(814405684581236774, SlashCommandPermissionType.ROLE, True)
     ]
 }
-class tags(commands.Cog):
+
+class Tags(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+        print(f'{self.__class__.__name__} Cog is ready')
     
-    @cog_ext.cog_subcommand(base="TagM", name="create", description="Create New Tag", guild_ids=guild_ids, base_default_permission=False,base_permissions=admin_perms, 
+
+    @cog_ext.cog_subcommand(base="Tag", name="create", description="Create New tag", guild_ids=guild_ids, base_default_permission=False, base_permissions=perms_a,
         options=[
-            create_option(name="tag_name", description="Give tag an name", required=True, option_type=3),
-            create_option(name="tag_message", description="Enter Tag message", required=True, option_type=3)
+            create_option(name="tag_name", description="Enter Tag Name", required=True, option_type=3)
         ])
-    async def create(self, ctx, tag_name, tag_message):
-        tag_filter = {'tag_name': tag_name, 'tag_guildId': ctx.guild.id}
-        result = await self.bot.tags.find_many_by_custom(tag_filter)
-        if result: return await ctx.send("There an tag already in database with this make use `/tag list` to ckeck all tags", hidden=True)
+    async def create(self, ctx, tag_name:str):
+        await ctx.defer()
+        await ctx.channel.send("Enter Your Tag Message")
+        try:
+            msg = await self.bot.wait_for('message', check= lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=60)
+        except asyncio.TimeoutError:
+            return await ctx.send('TimeoutError')
+        tag_filter = {'tag_name': tag_name, 'guildID': ctx.guild.id}
+        tag_data = {'made_by': ctx.author.id, 'tag_messsage': msg.content, 'time': datetime.datetime.now()}
 
-        tag_data = {'tag_message': tag_message, 'tag_creater': ctx.author.id, 'tag_createAt': datetime.datetime.utcnow()}
         await self.bot.tags.update_by_custom(tag_filter, tag_data)
-        await ctx.send("Your tag is add to the database")
+        data = await self.bot.tags.find_by_custom({'tag_name': tag_name, 'guildID': ctx.guild.id})
+        embed = discord.Embed(description=f"```py\n{data}\n```")
+        await ctx.send(embed=embed)
+    
+    @cog_ext.cog_subcommand(base="tag", name="use", description="user tag", guild_ids=guild_ids, base_default_permission=False, base_permissions=perms_staff,)
+    async def use(self, ctx, tag_name ,users:str=None, channel:str=None, extra:str=None):
+        await ctx.defer(hidden=True)
+        data = await self.bot.tags.find_by_custom({'tag_name': tag_name, 'guildID': ctx.guild.id})
+        if not data: return await ctx.send(f"No tag Found with name `{tag_name}`")
+        message = data['tag_messsage']
 
-    @cog_ext.cog_subcommand(base="Tag", name="list", description="Show list of all tags of the server", guild_ids=guild_ids, base_default_permission=False, base_permissions=staff_perms)
-    async def list(self, ctx):
-        tag_filter = {'tag_guildId': ctx.guild.id}
+        if users:
+            message = message.replace('{users}', f'{users}')
+        else:
+            message = message.replace('{users}', f'')
+
+        if channel:
+            message = message.replace('{channel}', f'{channel}')
+        else:
+            message = message.replace('{channel}', f'')
+        
+        if extra:
+            message = message.replace('{extra}', f'{extra}')
+        else:
+            message = message.replace('{extra}', f'')
+        await ctx.channel.send(message)
+        await ctx.send(f"You used tags named with {tag_name} in {ctx.channel.mention}", hidden=True)
+    
+    @cog_ext.cog_subcommand(base="Tag", description="Show list of all tags", name="list", guild_ids=guild_ids, base_default_permission=False, base_permissions=perms_staff)
+    async def _list(self, ctx):
+        await ctx.defer()
+        tag_filter = {'guildID': ctx.guild.id}
         data = await self.bot.tags.find_many_by_custom(tag_filter)
-        if not data: return await ctx.send("There is no tag for this server", hidden=True)
+        if not data: return await ctx.send("No Tags Found for this server.", hidden=True)
         pages = []
-        for tag in data:
-            user = self.bot.get_user(tag['tag_creater'])
+        for tags in data:
             description=f"""
-            Tag Name: {tag['tag_name']}
-            Tag made by: {user.name}
-            Tag Message: `{tag['tag_message']}`
-            Tag Created at: {tag['tag_createAt'].strftime("%I:%M %p %B %d, %Y")}
+            **Tage Name**: {tags['tag_name']}
+            **Tage Message**: {tags['tag_messsage']}
+            **Tage Made by**: <@{tags['made_by']}>
             """
             pages.append(description)
         
-        await Pag(title=f"List of Tags for {ctx.guild.name}", color=0x09f507, entries=pages, length=1).start(ctx)
+        await Pag(title=f"Tags list for {ctx.guild.name}", color=0x11eca4, length=1, entries=pages).start(ctx)
 
-    @cog_ext.cog_subcommand(base="Tag", description="Use tags", guild_ids=guild_ids, base_default_permission=False, base_permissions=staff_perms,
-        options=[
-            create_option("tag", description="Enter name of tag", option_type=3, required=True),
-            create_option("users", description="Enter user mentions if needed", option_type=3, required=False),
-            create_option("channel", description="Enter channel mentions if needed", option_type=3, required=False),
-            create_option("extra", description="Enter extra information if needed or leave it empty", option_type=3, required=False),
-        ])
-    async def tag(self, ctx, tag, users: str=None, channel: str=None, extra: str=None):
-        tag_filter = {'tag_name': tag, 'tag_guildId': ctx.guild.id}
-        data = await self.bot.tags.find_by_custom(tag_filter)
-        if not data: return await ctx.send("Please check your tag name there is no tag in databse with this name", hidden=True)
-        if users:
-            new_message = data['tag_message'].replace('{users}', f'{users}')
-        else:
-            new_message = data['tag_message'].replace('{users}', '')
+    @cog_ext.cog_subcommand(base="Tag", description="Edit an tag text", name="edit", guild_ids=guild_ids, base_default_permission=False, base_permissions=perms_a)
+    async def edit(self, ctx, tag_name:str):
+        await ctx.defer()
+        data =  await self.bot.tags.find_by_custom({'tag_name': tag_name, 'guildID': ctx.guild.id})
+        if not data: return await ctx.send(f"No tag found with name of `{tag_name}`")
+        await ctx.channel.send("Enter New Tag message")
+        try:
+            msg = await self.bot.wait_for('message', check= lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=60)
+        except:
+            return await ctx.send("TimeoutError")
         
-        if channel:
-            new_message = new_message.replace('{channel}', f'{channel}')
-        else:
-            new_message = new_message.replace('{channel}', f'')
-        
-        if extra:
-            new_message = new_message.replace('{extra}', f'{extra}')
-        else:
-            new_message = new_message.replace('{extra}', f'')        
+        await msg.add_reaction("✅")
+        embed = discord.Embed(title=f"Editing Tag {tag_name}", description=f"Are you sure you want to edit the tag `{tag_name}`",color=0xff0000)
+        embed.add_field(name="New Message:", value=msg.content)
+        embed.add_field(name="Old Message:", value=data['tag_messsage'],inline=False)
+        embed.timestamp = datetime.datetime.now()
+        buttons = [create_button(style=ButtonStyle.green, label="Yes", disabled=False), create_button(style=ButtonStyle.blurple, label="No", disabled=False)]
+        config = await ctx.send(embed=embed, components=[create_actionrow(*buttons)])
+        try:
+            res: ComponentContext = await wait_for_component(self.bot, components=[create_actionrow(*buttons)], check=lambda button_ctx: button_ctx.author == ctx.author, timeout=60)
+            if res.component['label'].lower() == "yes":
+                data['tag_messsage'] = msg.content
+                await self.bot.tags.upsert(data)
+                buttons = [create_button(style=ButtonStyle.green, label="Yes", disabled=True), create_button(style=ButtonStyle.blurple, label="No", disabled=True)]
+                await res.send('Your tag is updated', hidden=True)
+                return await config.edit(components=[create_actionrow(*buttons)])
 
-        await ctx.send("Done tag sended", hidden=True)
+            if res.component['label'].lower() == "no":
+                await res.send('no changes where made', hidden=True)
+                return await config.edit(content="Let me know you want to update the tag",embed=None, components=[create_actionrow(*buttons)])
 
-        await ctx.channel.send(new_message)
+        except asyncio.TimeoutError:
 
-    @cog_ext.cog_subcommand(base="Tag", name="Info", description="Show help command for the creating tags", guild_ids=guild_ids, base_default_permission=False)
+            buttons = [create_button(style=ButtonStyle.green, label="Yes", disabled=True), create_button(style=ButtonStyle.blurple, label="No", disabled=True)]
+            await config.edit(content="TimeOut Error", components=[create_actionrow(*buttons)])
+    
+    @cog_ext.cog_subcommand(base="Tag", name="Info", description="Show help command for the creating tags", guild_ids=guild_ids, base_default_permission=False, base_permissions=perms_staff)
     async def info(self, ctx):
         embed = discord.Embed(title="How to Use Tags", color=0x11eca4, description="ㅤㅤ\n")
         embed.add_field(name="How to Make Tag:",value="1. Select `/tagm create` command from list\n\n2. Select `tag_name` field and enter tag name\n\n3. Select `tag_message` field and enter your text.\n> To add mentions in tags add `{users}`\n> for channel `{channel}`, Extra info `{extra}`")
@@ -116,77 +149,6 @@ class tags(commands.Cog):
         embed.timestamp = datetime.datetime.utcnow()
         embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
         await ctx.send(embed=embed)
-
-    @cog_ext.cog_subcommand(base="TagM", name="edit", description="edit an tag", guild_ids=guild_ids, base_default_permission=False, base_permissions=admin_perms,
-        options=[
-            create_option(name="tag_name", description="Enter Tag Name", option_type=3, required=True),
-            create_option(name="new_message", description="Enter new Message for the tag", option_type=3, required=False),
-            create_option(name="remove_tag", description="Select this to delete tag", option_type=5, required=False)
-        ])
-    async def edit(self, ctx,tag_name: str, new_message=None,remove_tag: bool=None):
-        tag_filter = {'tag_name': tag_name, 'tag_guildId': ctx.guild.id}
-        data = await self.bot.tags.find_by_custom(tag_filter)
-        if not data: return await ctx.send(f"No tag found with `{tag_name}` try `/tag list` to get list of all tags")
-
-        if remove_tag == True:
-
-            buttons = [create_button(style=ButtonStyle.red, label="Yes", disabled=False), create_button(style=ButtonStyle.blurple, label="No", disabled=False)]
-
-            embed = discord.Embed(description=f"Are you sure you want to delete this tag\nTag Name: {data['tag_name']}\nTag Message: {data['tag_message']}\nMade by: <@{data['tag_creater']}>",color=0xff0000)
-
-            msg = await ctx.send(embed=embed, components=[create_actionrow(*buttons)], hidden=False)
-            while True:
-                try:
-                    button_ctx: ComponentContext = await wait_for_component(self.bot, components=create_actionrow(*buttons), timeout=10)
-
-                    if button_ctx.author == ctx.author:
-
-                        if button_ctx.component['label'].lower() == "yes":
-                            await self.bot.tags.delete(data['_id'])
-                            await button_ctx.edit_origin(content="Tag has been deleted", components=None)
-                            buttons = [create_button(style=ButtonStyle.red, label="Yes", disabled=True), create_button(style=ButtonStyle.blurple, label="No", disabled=True)]
-                            await msg.edit(components=[create_actionrow(*buttons)])
-                            return
-                        
-                        if button_ctx.component['label'].lower() == "no":
-                            buttons = [create_button(style=ButtonStyle.red, label="Yes", disabled=True), create_button(style=ButtonStyle.blurple, label="No", disabled=True)]
-                            await msg.edit(components=[create_actionrow(*buttons)])
-                            return
-                    else:
-                        await ctx.send("This is not your button")
-                except asyncio.TimeoutError:
-                    buttons = [create_button(style=ButtonStyle.red, label="Yes", disabled=True), create_button(style=ButtonStyle.blurple, label="No", disabled=True)]
-                    await msg.edit(components=[create_actionrow(*buttons)])
-                    break
-            return
-
-        if new_message:        
-
-            embed = discord.Embed(description=f"Are you sure you want to edit the tag `{tag_name}`",color=0xff0000)
-            embed.add_field(name="New Message", value=new_message)
-            embed.add_field(name="Old Message", value=data['tag_message'], inline=False)
-
-            buttons = [create_button(style=ButtonStyle.red, label="Yes", disabled=False), create_button(style=ButtonStyle.blurple, label="No", disabled=False)]
-            msg = await ctx.send(embed=embed, components=[create_actionrow(*buttons)])
-
-            while True:
-                try:
-                    button_ctx: ComponentContext = await wait_for_component(self.bot, components=create_actionrow(*buttons), timeout=10)
-                    if button_ctx.author == ctx.author:
-                        if button_ctx.component['label'].lower() == "yes":
-                            data['tag_message'] = new_message
-                            await self.bot.tags.upsert(data)
-                            await button_ctx.send('Tags has been updated',hidden=False)
-                            buttons = [create_button(style=ButtonStyle.red, label="Yes", disabled=True), create_button(style=ButtonStyle.blurple, label="No", disabled=True)]
-                            return await msg.edit(components=[create_actionrow(*buttons)])
-                        if button_ctx.component['label'].lower() == "no":
-                            await ctx.send("command is cancelled")
-                            buttons = [create_button(style=ButtonStyle.red, label="Yes", disabled=True), create_button(style=ButtonStyle.blurple, label="No", disabled=True)]
-                            return await msg.edit(components=[create_actionrow(*buttons)])
-                    else:
-                        await button_ctx.send("This buttons are not for your", hidden=True)
-                except asyncio.TimeoutError:
-                        buttons = [create_button(style=ButtonStyle.red, label="Yes", disabled=True), create_button(style=ButtonStyle.blurple, label="No", disabled=True)]
-                        return await msg.edit(content="Timeouted",components=[create_actionrow(*buttons)])
 def setup(bot):
-    bot.add_cog(tags(bot))
+    bot.add_cog(Tags(bot))
+
